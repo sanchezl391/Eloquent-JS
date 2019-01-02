a3a609d4d8331b4adb0918bbf757733443acf45d
Luis Sanchez <sanchezl391@gmail.com>
2019-01-02 12:38:13 -0700

    finished chapter 2 exercises

---
+++ Exercises/02_program_structure.js
@@ -0,0 +1,70 @@
+// Looping a triangle
+// Write a loop that makes seven calls to console.log to output the following triangle:
+
+// #
+// ##
+// ###
+// ####
+// #####
+// ######
+// #######
+
+for(let i = 0;i < 7;i++) {
+    let str = '';
+    for (let b = 0; b < i + 1;b++) {
+        str+='#';
+    }
+    console.log(str);
+}
+
+// Better Sol: 
+// for (let line = "#"; line.length < 8; line += "#")
+//   console.log(line);
+
+// Fizz Buzz
+// Write a program that uses console.log to print all the numbers from 1 to 100, 
+// with two exceptions. For numbers divisible by 3, print "Fizz" instead of the
+//  number, and for numbers divisible by 5 (and not 3), print "Buzz" instead.
+
+// When you have that working, modify your program to print "FizzBuzz" for 
+// numbers that are divisible by both 3 and 5 (and still print "Fizz" or "Buzz"
+//  for numbers divisible by only one of those).
+
+for(let i = 1 ;i <= 100 ;i++) {
+    let str = '';
+
+    if(i % 3 == 0)
+        str+='Fizz';
+    if(i % 5 == 0)
+        str+='Buzz';
+    if(str)
+        console.log(i + str);
+}
+
+// Chessboard
+// Write a program that creates a string that represents an 8×8 grid, using newline characters to separate lines. At each position of the grid there is either a space or a "#" character. The characters should form a chessboard.
+
+// Passing this string to console.log should show something like this:
+
+//  # # # #
+// # # # # 
+//  # # # #
+// # # # # 
+//  # # # #
+// # # # # 
+//  # # # #
+// # # # #
+// When you have a program that generates this pattern, define a binding size = 8 and change the program so that it works for any size, outputting a grid of the given width and height.
+let str = '';
+for(let i = 1;i <= 8;i++) {
+    for(let b = 1;b <= 8;b++) {
+        if((i + b) % 2 === 0)
+            str+='#';
+        else 
+            str+=' ';   
+    }
+    str+='\n';
+}
+console.log(str);
+
+
